683aeccc1561f7b427a471a8145fb0545f845819
mittt777 <121394585+mittt777@users.noreply.github.com>
2025-03-11 17:16:06 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
 import { Hono } from 'hono';
+import {streamSSE } from 'hono/streaming';
+import postgres from "postgres";
 
-const app = new Hono();
+const sql = postgres(
+  "postgresql://postgres.nfzomzhmgexrwhdgpexo:GqSrLZbwWtIrwy7H@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
+  //"postgresql://postgres:GqSrLZbwWtIrwy7H@db.nfzomzhmgexrwhdgpexo.supabase.co:5432/postgres",
+);
+//"postgresql://postgres.nfzomzhmgexrwhdgpexo:GqSrLZbwWtIrwy7H@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
+const app = new Hono()
 
+app.get('/ss', async (c) => {
+  const resp = await sql`
+    SELECT * 
+    FROM test_table`
+  return c.json(resp)
+})
+//const app = new Hono();
+app.get('/sse', async (c) => {
+  let id = 0;
+  return streamSSE(c, async (stream) => {
+    let ended = false;
+    while (true) {
+      stream.onAbort(() => {
+        if (!ended) {
+          ended = true
+          console.log('Stream aborted')
+        }
+      })
+      const message = `It is ${new Date().toISOString()}`
+      await stream.writeSSE({
+        data: String(id++)
+      })
+      await stream.sleep(100)
+    }
+  })
+})
+
+app.get("/api/hello", (c) => {
+  return c.json({
+    ok: true,
+    message: "Hello Hono!",
+  });
+});
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
